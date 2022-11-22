7a6edec37c1857ed2c6a263fed571bf573940f63
1SM443L <micalyson.l@escolar.ifrn.edu.br>
2022-11-22 10:21:13 -0300

    Agora sim ta quase pronto

--- formulario/admin.py
+++ formulario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from formulario.models import Curso, Aluno, MiniCursos
 
 # Register your models here.
+
+admin.site.register(Curso) 
+admin.site.register(MiniCursos)
+admin.site.register(Aluno)
